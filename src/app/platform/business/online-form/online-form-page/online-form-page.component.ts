1f8865de5844607ec248a923e4fc48fbace75c60
Dzmitry Shmat <dzmitry.shmat@aterise.com>
2023-06-19 02:52:37 +0400

    business client registration form

--- src/app/platform/business/online-form/online-form-page/online-form-page.component.ts
+++ src/app/platform/business/online-form/online-form-page/online-form-page.component.ts
@@ -1,4 +1,6 @@
 import {Component} from '@angular/core';
+import {UntypedFormBuilder, Validators} from '@angular/forms';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-online-form-page',
@@ -7,4 +9,28 @@ import {Component} from '@angular/core';
 })
 export class OnlineFormPageComponent {
 
+  entityForm = this.fb.group({
+    fullName: [null, [Validators.required, Validators.maxLength(50)]],
+    shortName: [null, [Validators.required, Validators.maxLength(50)]],
+    accNumber: [null, [Validators.required, Validators.maxLength(50)]],
+    registrationAuthority: [null, [Validators.required, Validators.maxLength(50)]],
+    registrationNumber: [null, [Validators.required, Validators.maxLength(50)]],
+    registrationDate: [null, [Validators.required, Validators.maxLength(50)]],
+  })
+
+  legalForm = this.fb.group({
+    country: [null, [Validators.required, Validators.maxLength(30)]],
+    locality: [null, [Validators.required, Validators.maxLength(30)]],
+    region: [null, [Validators.required, Validators.maxLength(30)]],
+    district: [null, [Validators.required, Validators.maxLength(10)]],
+    localityType: [null, [Validators.required, Validators.maxLength(10)]],
+    zip: [null, [Validators.required, Validators.maxLength(50)]],
+    street: [null, [Validators.required, Validators.maxLength(20)]],
+    house: [null, [Validators.required, Validators.maxLength(50)]],
+    building: [null, [Validators.required, Validators.maxLength(50)]],
+    office: [null, [Validators.required, Validators.maxLength(50)]],
+  })
+
+  constructor(private fb: UntypedFormBuilder, private router: Router) {
+  }
 }
--- src/app/platform/business/online-form/online-form.module.ts
+++ src/app/platform/business/online-form/online-form.module.ts
@@ -2,6 +2,7 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {OnlineFormPageComponent} from './online-form-page/online-form-page.component';
 import {OnlineFormRoutingModule} from './online-form-routing.module';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 
 @NgModule({
@@ -10,7 +11,9 @@ import {OnlineFormRoutingModule} from './online-form-routing.module';
   ],
   imports: [
     CommonModule,
-    OnlineFormRoutingModule
+    OnlineFormRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class OnlineFormModule {

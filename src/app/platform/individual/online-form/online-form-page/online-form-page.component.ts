cba6e9460f05d337cd0a8b2ad7fdf45e527a8acd
Dzmitry Shmat <dzmitry.shmat@aterise.com>
2023-06-20 02:36:10 +0400

    little adjustments

--- src/app/platform/individual/online-form/online-form-page/online-form-page.component.ts
+++ src/app/platform/individual/online-form/online-form-page/online-form-page.component.ts
@@ -12,13 +12,13 @@ export class OnlineFormPageComponent {
   personalForm = this.fb.group({
     surname: ['Anderson', [Validators.required, Validators.maxLength(30)]],
     name: ['Jason', [Validators.required, Validators.maxLength(30)]],
-    citizenship: [null, [Validators.required, Validators.maxLength(30)]],
+    citizenship: ['Germany', [Validators.required, Validators.maxLength(30)]],
     dob: ['1990-10-18', [Validators.required, Validators.maxLength(10)]],
     pob: ['Philadelphia, PA, USA', [Validators.required, Validators.maxLength(50)]],
   })
 
   contactForm = this.fb.group({
-    docType: [null, [Validators.required, Validators.maxLength(30)]],
+    docType: ['Passport of the citizen of USA', [Validators.required, Validators.maxLength(30)]],
     docNumber: ['HB3413812', [Validators.required, Validators.maxLength(30)]],
     idNumber: ['HB3413812', [Validators.required, Validators.maxLength(30)]],
     issueDate: ['1990-10-18', [Validators.required, Validators.maxLength(10)]],
